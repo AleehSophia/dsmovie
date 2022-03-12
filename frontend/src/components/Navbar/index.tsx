3e0a79a5fabc31895136088dac77fb89483aaec8
Alessia Casado <alessia.casado@gmail.com>
2022-03-12 12:29:15 -0300

    github navbar name fix

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -11,7 +11,7 @@ function Navbar() {
                 <a href="https://github.com/AleehSophia" target="_blank" rel="noreferrer">
                   <div className="dsmovie-contact-container">
                       <GithubIcon />
-                      <p className="dsmovie-contact-link">/devsuperior</p>
+                      <p className="dsmovie-contact-link">/AleehSophia</p>
                   </div>
               </a>
           </div>
